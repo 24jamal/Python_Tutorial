991a1e7dd05ac964a8de4ff25e8ff28990f02e65
Mohamed Jamaludeen <jamaludeen750@gmail.com>
2025-02-26 21:26:35 +0530

    first commit

---
+++ Python_Tutorial.py
@@ -0,0 +1,145 @@
+#Reference : https://www.geeksforgeeks.org/getting-started-with-python-programming/?ref=lbp
+
+# print("Hello world")
+
+# name = "Jamal"
+
+# print("Hello : "+ name)
+
+#-------------------------------------------
+# """ 
+# Multiline Comment
+# """
+# # printing string with \
+# sentence = "Tiger is a national animal of India\
+#     It is Fearless in Nature. Its main food is Fats and Proteins."
+
+# #printing numbers sum with \
+# total = 1+2+456+4+56+\
+#     +45+100
+# print(sentence)
+
+# print(total)
+
+#------------------------------------------------
+
+# numbers = [1,2,3,4,5,6]
+
+# print(numbers)
+
+# Getting input from user
+
+# name = input("Enter your name")
+# print(f"Hello :"+ name)
+
+# x =5
+# x = "Samantha"
+
+# print(x)
+#-----------------------------------------------------------
+# # Typecast string to float
+# num = float("23")
+# print(num)
+
+
+# Typecast int to string
+
+# num = 10
+
+# stringnum  = str(num)
+# print(stringnum)
+
+#-------------------------------------------------
+
+# Check their datatypes
+
+# myint = 10
+# myfloat = 10.20
+# mystring = "My string"
+# myBool = True
+# mylist = [1,2,3,4]
+# mydict = {"a": "apple"}
+# mytuple = ("jelly","gum","juice")
+# myset = set(mydict)
+
+# myset2 = {2,3,4,1,1,2}
+
+# print(type(myint))
+# print(type(myfloat))
+# print(type(mystring))
+# print(type(myBool))
+# print(type(mydict))
+# print(type(mylist))
+# print(type(myset))
+# print(type(mytuple))
+# print(myset)
+# print(myset2)
+
+#------------------------------------------------
+# Defining a function
+# def printMyname(name):
+#     print("Hello :"+name)
+
+
+# printMyname("Jamal")
+
+#------------------------------------------------------
+
+#Delete a variable
+
+# x = 10
+# print(x)
+# del x
+
+# #------------------------------------------------------------
+
+# #Swapping Variable
+
+# a,b = 10,20
+# a,b = b,a
+# print(a,b)
+
+#---------------------------------------------------------
+
+# #Length of string
+
+# word = "hello"
+# size = len(word)
+
+# print("length of word : ", size)
+# #------------------------------------------------------------
+# #python keywords
+# import keyword
+
+# print(keyword.kwlist)
+
+#------------------------------------------------------
+#boolean
+
+# print(True == 1)
+# print (True +True + True)
+
+# print(True + False + True)
+# print(None == 0)
+
+# print(None == [])
+
+#---------------------------------------------------------
+#Logic Operators
+
+# a = True
+# b = False
+
+# # And Needs both True for True
+# print("and Logic", (a and b))
+
+# # OR Needs one True for True
+# print("or logic : ",(a or b))
+
+# # not logic gives opposite given bool
+
+# print(not a)
+
+#---------------------------------------------------------------
+
+#Membership Opearator
